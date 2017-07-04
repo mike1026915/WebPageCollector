84d1e3227660ffa84ca8d7d48cf019090d96299f
Mike Fang (RD-TW) <mike_fang@tw.trendnet.org>
2017-07-04 18:12:10 +0800

    Note the fetched URLs in google search page

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ keyword = ['requests save html']
 query = {'q': 'requests save html'}
 
 r = requests.get(search_engine, params=query)
-bs = BeautifulSoup(r.text)
+bs = BeautifulSoup(r.text, "lxml")
+for c in  bs.find_all("a"):
+
+    if "31126596" in c.attrs['href']:
+        print c.attrs['href']
