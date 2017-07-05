9c72588fd7d59ce80af35d44d5beb02c078dc387
Mike Fang (RD-TW) <mike_fang@tw.trendnet.org>
2017-07-05 18:07:25 +0800

    Save the webpage

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import re
-
+import os
+import hashlib
 import requests
 from bs4 import BeautifulSoup
 search_engine = 'https://www.google.com.tw/search'
-keywords = ['requests save html', 'JavaScript', 'photos', "web script", "automation", 'Python', "news", "weather"]
+keywords = ['requests save html',] #['requests save html', 'JavaScript', 'photos', "web script", "automation", 'Python', "news", "weather"]
 excludes = ['webcache.googleusercontent.com']
 
 google_re_pattern = r"(/url\?q=)?(https?://[^&]*)(&.*)*"
@@ -13,8 +14,6 @@ for keyword in keywords:
     query = {'q': keyword}
     #head = requests.head(search_engine, params=query)
     get = requests.get(search_engine, params=query)
-    if "html" not in get.headers['Content-Type']:
-        continue
     bs = BeautifulSoup(get.text, "lxml")
 
     for a in  bs.find_all("a"):
@@ -28,7 +27,23 @@ for keyword in keywords:
             except Exception:
                 continue
 
-print all_url_set, len(all_url_set)
+if not os.path.isdir("result"):
+    os.makedirs("result")
+
+sha_1 = hashlib.sha1()
+
+for url in all_url_set:
+    get = requests.get(url, params=query)
+    if "html" not in get.headers['Content-Type']:
+        continue
+    get.encoding = "utf8"
+    print url
+    sha_1.update(repr(get.text))
+    file_name = sha_1.hexdigest() #url.split("/")[-1] if not url.endswith('/') else url.split("/")[-2]
+    file_path = os.path.join("result", file_name+".html")
+    with open(file_path, "w") as f:
+        f.write(get.text.encode("utf8"))
+
 
 """
 /url?q=https://stackoverflow.com/questions/31126596/saving-response-from-requests-to-file&sa=U&ved=0ahUKEwjAlOqAsO_UAhWIKJQKHXyBCzMQFggTMAA&usg=AFQjCNGaUwPNQPOPgkutjrIpE2i2vVp0Rg
